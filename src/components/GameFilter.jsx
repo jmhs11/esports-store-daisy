266655c799af455269cf9ca1ae061bf4a8583111
jmhs11 <josemariahs@gmail.com>
2022-08-02 15:23:20 +0200

    Update gameFilter

--- src/components/GameFilter.jsx
+++ src/components/GameFilter.jsx
@@ -24,20 +24,32 @@ const GameFilter = () => {
 					Popular
 				</option>
 				<option value={'bestsellers'}>BestSellers</option>
-				<option>SpecialOffers</option>
+				<option value={'specialoffers'}>SpecialOffers</option>
 			</select>
 
-			<div>
+			<div className='flex flex-col gap-2'>
 				{gameList.map(game => (
-					<div
-						key={game.id}
-						className='grid grid-cols-4 rounded overflow-hidden'
-					>
-						<img src={game.image} alt={game.name} />
+					<div key={game.id} className='grid grid-cols-4 rounded'>
+						<img src={game.image} alt={game.title} />
 						<div className='flex gap-2 justify-around items-center border-2 border-l-0 col-start-2 col-end-5 rounded rounded-l-none'>
 							<h3 className='text-xl font-bold'>{game.title}</h3>
 							<p>{game.price}</p>
-							<DotsHorizontalIcon className='h-6 w-6' />
+							<div className='dropdown dropdown-top dropdown-end'>
+								<label tabIndex='0' className='btn btn-ghost btn-circle'>
+									<DotsHorizontalIcon className='h-6 w-6' />
+								</label>
+								<ul
+									tabIndex='0'
+									className='menu dropdown-content mt-3 p-2 shadow bg-base-100 border rounded-box'
+								>
+									<li></li>
+									<div className='divider m-0'></div>
+									<li></li>
+									<li></li>
+									<div className='divider m-0'></div>
+									<li></li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				))}
